0201d7f725adccef1406d8990dde7c4423ed1e6a
Rafael Tavares <rafaeltavares91@gmail.com>
2019-03-07 01:16:13 -0300

    creating application domain

---
+++ src/main/java/com/recipe/domain/Notes.java
@@ -0,0 +1,22 @@
+package guru.springframework.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String recipeNotes;
+
+    @OneToOne
+    private Recipe recipe;
+
+}
